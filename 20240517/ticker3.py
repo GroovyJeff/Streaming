4e4c1817e9c8ed8bca0166672d1afb8ac7e224eb
Jeff Groves <jeffrey.groves@mail.com>
2024-05-21 10:43:51 -0600

    Can now count

--- 20240517/ticker3.py
+++ 20240517/ticker3.py
@@ -1,5 +1,11 @@
 import tkinter as tk
 
+arr = list(range(20));
+
+i=0;
+
+print(arr);
+
 root = tk.Tk()
 root.geometry("1600x100+0+100")
 root.overrideredirect(True)
@@ -7,9 +13,17 @@ root.overrideredirect(True)
 root.label = tk.Label(text="Hello")
 root.label.pack()
 
+def update_label():
+    global arr, i
+    root.label.config(text=str(arr[i]))
+    i = i+1;
+    root.after(1000, update_label)
+
+root.after(1000, update_label)
+
 root.mainloop()
 
-for i in range(100000):
-    root.label.config(text=str(i))
+# for i in range(100000):
+#    root.label.config(text=str(i))
 
 
