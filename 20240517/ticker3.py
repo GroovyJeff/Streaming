066cfa374ad2995b17757ded7d1ba99a370601f3
Jeff Groves <jeffrey.groves@mail.com>
2024-05-21 11:23:24 -0600

    Now understand how to print time zone

--- 20240517/ticker3.py
+++ 20240517/ticker3.py
@@ -1,10 +1,52 @@
 import tkinter as tk
+import datetime
+import pytz
 
-arr = list(range(20));
+tzone = pytz.timezone("EST5EDT");
+print(datetime.datetime.now().astimezone(tzone).strftime("%H%M,%a%d%b"))
+
+
+exit();
+
+# arr = list(range(20));
+
+arr = [];
 
 i=0;
 
-print(arr);
+zones = {
+          "ASamoa": "Pacific/Pago_Pago",
+          "HST": "Pacific/Honolulu",
+          "PT": "US/Pacific",
+          "MT":  "US/Mountain",
+          "CT": "US/Central",
+          "ET": "US/Eastern",
+          "GMT": "GMT",
+          "Lagos": "Africa/Lagos",
+          "Milan": "Europe/Rome", 
+          "Cairo": "Africa/Cairo",
+          "Delhi": "Asia/Kolkata",
+          "Jakarta": "Asia/Jakarta",
+          "HongKong": "Asia/Hong_Kong", 
+          "Manila": "Asia/Manila",
+          "Tokyo": "Asia/Tokyo",
+          "Brisbane": "Australia/Brisbane",
+          "Sydney": "Australia/Sydney",
+          "Auckland": "Pacific/Auckland",
+          "Chatam":"Pacific/Chatham",
+          "Samoa": "Pacific/Apia"
+}
+
+for i in zones:
+    
+    current_time = datetime.datetime.now(datetime.timezone.utc).astimezone(zones[i])
+    tz = pytz.timezone(zones[i])
+    print(current_time.strftime("%H%M,%a%d%b"))
+
+#    zstr = zones[i]+ " "+
+#    print(i, zones[i])
+
+exit()
 
 root = tk.Tk()
 root.geometry("1600x100+0+100")
